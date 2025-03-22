25642a48482c2e694427bc8475604a7a7d4a22b5
kostadinoff <drkostadinkostadinov@gmail.com>
2025-03-22 20:07:20 +0200

    modified:   kostadin-default.R

--- kostadin-default.R
+++ kostadin-default.R
@@ -709,6 +709,42 @@ krk_reg <- function(data, outcome, predictors, log_outcome = FALSE, custom_formu
   bind_rows(univariate_results, multivariate_results)
 }
 
+kkonehot <- function(data, column) {
+  # Check if input is a tibble
+  if (!inherits(data, "tbl_df")) {
+    stop("Input 'data' must be a tibble")
+  }
+
+  # Check if column exists in data
+  if (!column %in% names(data)) {
+    stop("Specified column not found in the dataset")
+  }
+
+  # Get the variable
+  var <- data[[column]]
+
+  # Check if variable is character or factor
+  if (!(is.character(var) || is.factor(var))) {
+    stop("Column must be character or factor type for one-hot encoding")
+  }
+
+  # Convert character to factor if needed
+  if (is.character(var)) {
+    var <- as.factor(var)
+  }
+
+  # Get levels of the factor
+  levels <- levels(var)
+
+  # Create one-hot encoded columns
+  for (level in levels) {
+    new_col_name <- paste0(column, "_", level)
+    data[[new_col_name]] <- ifelse(var == level, 1, 0)
+  }
+
+  # Return the modified dataset
+  return(data)
+}
 
 ###### ---Define theme---######
 
